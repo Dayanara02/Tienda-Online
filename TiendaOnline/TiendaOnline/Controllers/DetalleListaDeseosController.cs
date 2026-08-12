088bf4db59c874931853639100277c38b334a279
lopezmeydelline-cloud <lopezmeydelline@gmail.com>
2026-08-11 21:51:13 -0600

    Comentarios

--- TiendaOnline/TiendaOnline/Controllers/DetalleListaDeseosController.cs
+++ TiendaOnline/TiendaOnline/Controllers/DetalleListaDeseosController.cs
@@ -5,14 +5,17 @@ using Microsoft.AspNetCore.Authorization;
 using TiendaOnline.Dominio.Entidades;
 
 namespace TiendaOnline.API.Controllers;
-
+//Permite el acceso al controlador unicamente a usuarios autenticados 
 [Authorize]
 [ApiController]
 [Route("api/[controller]")]
+
+//Define la ruta base del controlador
 public class DetalleListaDeseosController : ControllerBase
 {
     private readonly TiendaOnlineContext _context;
-
+    //Constructor del controlador 
+    //Recibe el contexto de la base de datos mediante inyeccion de dependencias
     public DetalleListaDeseosController(
         TiendaOnlineContext context)
     {
@@ -59,7 +62,8 @@ public class DetalleListaDeseosController : ControllerBase
             .AnyAsync(d =>
                 d.IdListaDeseos == detalle.IdListaDeseos &&
                 d.IdProducto == detalle.IdProducto);
-
+        //Si el producto ya existe, devuelve un error 409 Conflict
+        //Indicando que no se puede agregar nuevamente 
         if (existe)
         {
             return Conflict(
@@ -82,6 +86,7 @@ public class DetalleListaDeseosController : ControllerBase
     }
 
     // PUT: api/DetalleListaDeseos/1/5
+    //Permite actualizar un detalle existente de la lista de deseos 
     [HttpPut("{idListaDeseos}/{idProducto}")]
     public async Task<IActionResult> PutDetalleListaDeseo(
         int idListaDeseos,
@@ -108,7 +113,7 @@ public class DetalleListaDeseosController : ControllerBase
     public async Task<IActionResult> DeleteDetalleListaDeseo(
         int idListaDeseos,
         int idProducto)
-    {
+    {   //Busca el detalle utilizando el ID de la lista de deseos 
         var detalle = await _context.DetalleListaDeseos
             .FindAsync(idListaDeseos, idProducto);
 
@@ -119,7 +124,7 @@ public class DetalleListaDeseosController : ControllerBase
 
         _context.DetalleListaDeseos.Remove(detalle);
         await _context.SaveChangesAsync();
-
+        //Indica que la eliminacion se realizo correctamente 
         return NoContent();
     }
 }
