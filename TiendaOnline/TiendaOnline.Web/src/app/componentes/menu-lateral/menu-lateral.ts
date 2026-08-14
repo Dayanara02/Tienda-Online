b8f4ddc3692286b8e5c2bab93d35a68d795f888d
Dayanara02 <dayanaraporras861@gmail.com>
2026-08-13 18:34:08 -0600

    cambios para mejoras

--- TiendaOnline/TiendaOnline.Web/src/app/componentes/menu-lateral/menu-lateral.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-menu-lateral',
-  imports: [],
-  templateUrl: './menu-lateral.html',
-  styleUrl: './menu-lateral.css',
-})
-export class MenuLateral {}
--- TiendaOnline/TiendaOnline.Web/src/app/componentes/tarjeta-producto/tarjeta-producto.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-tarjeta-producto',
-  imports: [],
-  templateUrl: './tarjeta-producto.html',
-  styleUrl: './tarjeta-producto.css',
-})
-export class TarjetaProducto {}
